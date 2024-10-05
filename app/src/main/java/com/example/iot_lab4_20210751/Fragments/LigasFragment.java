fd43c1a15bdb80954567e6d8838e08e6c904e1e8
Santy2304 <a20210751@pucp.edu.pe>
2024-10-04 19:22:27 -0500

    creaciones

---
+++ app/src/main/java/com/example/iot_lab4_20210751/Fragments/LigasFragment.java
@@ -0,0 +1,21 @@
+package com.example.iot_lab4_20210751.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.iot_lab4_20210751.R;
+
+public class LigasFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ligas, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/iot_lab4_20210751/Service/FootballService.java
@@ -0,0 +1,4 @@
+package com.example.iot_lab4_20210751.Service;
+
+public interface FootballService {
+}
